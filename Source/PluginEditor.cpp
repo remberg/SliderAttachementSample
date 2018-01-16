db7e51797ac82bd8f8b03ba8898f6de71fe69d56
remberg <web@sr-media.info>
2018-01-16 07:54:30 +0100

    Added some comments

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -12,13 +12,18 @@ DemoPluginAudioProcessorEditor::DemoPluginAudioProcessorEditor (DemoPluginAudioP
     slider1->setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
     slider1->setTextBoxStyle(Slider::TextBoxBelow, false, 40, 15);
     attachmentSlider1 = new SliderAttachment(p.parameters, "YourParameterID", *slider1);
-
+    
+    //slider2
     addAndMakeVisible(slider2 = new Slider());
     slider2->setSliderStyle(Slider::RotaryHorizontalVerticalDrag);
     slider2->setTextBoxStyle(Slider::TextBoxBelow, false, 40, 15);
     attachmentSlider2 = new SliderAttachment(p.parameters, "YourParameterID", *slider2);
+   
+    //make the plugin Resizable
     setResizable(true, true);
+    //set FixedAspectRatio (widthOverHeight)
     getConstrainer()->setFixedAspectRatio(2);
+    //set the plugin resize limits
     setResizeLimits(600, 300, 900, 450);
 }
 
@@ -30,11 +35,11 @@ DemoPluginAudioProcessorEditor::~DemoPluginAudioProcessorEditor()
 void DemoPluginAudioProcessorEditor::paint (Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
 }
 
 void DemoPluginAudioProcessorEditor::resized()
 {
+    //Set the sliders x,y,width,height ralative to its parent size
     slider1->setBoundsRelative  (0.25, 0.2, 0.2, 0.4);
     slider2->setBoundsRelative  (0.50, 0.2, 0.2, 0.4);
 }
